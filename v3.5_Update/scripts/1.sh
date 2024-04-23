542904af0ba043aaa41a67034355f3cc22bd38c3
sudo-000 <happy_mimimix@163.com>
2024-04-23 16:09:07 -0500

    Signed-off-by: sudo-000 <happy_mimimix@163.com>

---
+++ v3.5_Update/scripts/1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source '/root/Desktop/v3.5 Update Combo/scripts/pkgutils.sh'
+trap 'error' ERR
+set +e
+
+kdialog --title "Install v3.5 Update Combo" --error "This tool will guide you through the installation process of the unofficial v3.5 update for Red Star OS 3.0. \n\nThis will upgrade the system kernel to 5.4 x86_64 along with updates to many other critical system components and libraries including but not limited to gcc and yum. \n\nThe process is automatic, do not touch anything except typing your login password when asked. \nYour device will reboot for a couple of times during the process, it's recommended to set up automatic login in \"System Preferences -> Accounts -> Login Options -> Automatic Login\" so the amount of times the password need to be typed can be decreased. \n\nClick 'OK' when ready... "
+set -x
+rm -rf /workspace
+mkdir /workspace
+cd /workspace
+Install bc-1.07.1 gz
+InstallKernel 3.19.8 gz
+echo "[Desktop Entry]" > '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Encoding=UTF-8" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Type=Application" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Exec=konsole -e bash -c \"cd /root/Desktop/v3.5\ Update\ Combo;  ./scripts/2.sh\"" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Terminal=false" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Name=v3.5 Update Combo" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Categories=Applocation" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+set +x
+trap '' ERR
+for ((i = 5; i > 0; i--)); do
+    echo -ne "Press any key in $i to abort automatic reboot... \r"
+    read -rs -n 1 -t 1 key
+    if [[ $key ]]; then
+        echo -e "\nReboot aborted. "
+        sleep 1
+        exec bash
+    fi
+done
+echo -e "\nRebooting now... "
+sleep 1
+reboot
---
+++ v3.5_Update/scripts/3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+source '/root/Desktop/v3.5 Update Combo/scripts/pkgutils.sh'
+trap 'error' ERR
+set +e
+
+set -x
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/:/lib
+cd /workspace
+Extract glibc-2.29 xz
+mkdir build
+cd build
+../configure --prefix=/lib
+make -j$(cat /proc/cpuinfo | grep "processor" | wc -l)
+make Install
+cd ..
+CleanUp glibc-2.29
+exec bash
+export FORCE_UNSAFE_CONFIGURE=1
+Install coreutils-8.32 xz
+Install gdb-8.3 xz
+echo "[Desktop Entry]" > '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Encoding=UTF-8" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Type=Application" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Exec=konsole -e bash -c \"cd /root/Desktop/v3.5\ Update\ Combo;  ./scripts/3.sh\"" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Terminal=false" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Name=v3.5 Update Combo" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+echo "Categories=Applocation" >> '/root/Desktop/v3.5 Update Combo/scripts/next.desktop'
+set +x
+trap '' ERR
+for ((i = 5; i > 0; i--)); do
+    echo -ne "Press any key in $i to abort automatic reboot... \r"
+    read -rs -n 1 -t 1 key
+    if [[ $key ]]; then
+        echo -e "\nReboot aborted. "
+        sleep 1
+        exec bash
+    fi
+done
+echo -e "\nRebooting now... "
+sleep 1
+reboot
